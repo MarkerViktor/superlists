209852c272d7234e4c831da4d085659b7fc2708b
markerviktor <MarkerViktor@outlook.com>
2020-06-11 14:14:57 +0700

    Новый URL, редставление и шаблон для ввывода списка

--- lists/views.py
+++ lists/views.py
@@ -8,6 +8,11 @@ def home_page(request):
     '''домашняя страница'''
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect('/lists/personal-list')
+    return render(request, 'home.html')
+
+
+def view_lists(request):
+    '''представление списка'''
     all_items = Item.objects.all()
-    return render(request, 'home.html', {'items': all_items})
+    return render(request, 'list.html', {'items': all_items})
--- superlists/urls.py
+++ superlists/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls import url
 from lists import views
 
 urlpatterns = [
-    url(r'^$', views.home_page, name='home')
+    url(r'^$', views.home_page, name='home'),
+    url(r'^lists/personal-list/$', views.view_lists, name='view_lists')
 ]
