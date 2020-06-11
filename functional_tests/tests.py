921290783d546744a6c7d0e5e1c066f24c8644cf
markerviktor <MarkerViktor@outlook.com>
2020-06-12 00:06:14 +0700

    Первые шаги ФТ для макетирования + стилевого оформления

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,7 +4,7 @@ from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
 
-MAX_WAIT = 3
+MAX_WAIT = 1
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -113,3 +113,30 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Купить молоко', page_text)
 
         # Удовлетворенные они оба ложатся спать
+
+    def test_layout_and_styling(self):
+        """тест макета и стилевого оформления"""
+        # Эдит открывает домашнюю страницу
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # Она замечает, что поле ввода аккуратно центрировано
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            first=inputbox.location['x'] + inputbox.size['width'] / 2,
+            second=512,
+            delta=10
+        )
+
+        # Она начинает новый список и видит, что поле ввода там тоже аккуратно центрировано
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            first=inputbox.location['x'] + inputbox.size['width'] / 2,
+            second=512,
+            delta=10
+        )
+
