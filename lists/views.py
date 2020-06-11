8f6ff8693a8ddf10e6c08db79430f6c0cb1f3ca6
markerviktor <MarkerViktor@outlook.com>
2020-06-11 14:42:47 +0700

    Изменение action для форм, новый URL и тесты

--- lists/views.py
+++ lists/views.py
@@ -5,10 +5,6 @@ from lists.models import Item
 
 
 def home_page(request):
-    '''домашняя страница'''
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/personal-list')
     return render(request, 'home.html')
 
 
@@ -16,3 +12,9 @@ def view_lists(request):
     '''представление списка'''
     all_items = Item.objects.all()
     return render(request, 'list.html', {'items': all_items})
+
+
+def new_list(request):
+    '''новый список'''
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/personal-list/')
--- superlists/urls.py
+++ superlists/urls.py
@@ -5,5 +5,7 @@ from lists import views
 
 urlpatterns = [
     url(r'^$', views.home_page, name='home'),
-    url(r'^lists/personal-list/$', views.view_lists, name='view_lists')
+    url(r'^lists/personal-list/$', views.view_lists, name='view_lists'),
+    url(r'^lists/new$', views.new_list, name='new_list'),
+
 ]
