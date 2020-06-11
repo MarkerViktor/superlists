f1b228a8530a7eee47a8a1ffb601e45b6fcc7379
markerviktor <MarkerViktor@outlook.com>
2020-06-12 00:07:27 +0700

    .

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,16 +1,9 @@
-from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url
-from lists import views
+from django.conf.urls import include
+from lists import views as list_views
+from lists import urls as list_urls
 
 urlpatterns = [
-    path('', views.home_page, name='home'),
-    path('lists/new', views.new_list, name='new_list'),
-    path('lists/<int:list_id>/', views.view_list, name='view_lists'),
-    path('lists/<int:list_id>/add_item', views.add_item, name='add_item'),
-
-    # url(r'^$', views.home_page, name='home'),
-    # url(r'^lists/new$', views.new_list, name='new_list'),
-    # url(r'^lists/(\d+)/$', views.view_list, name='view_lists'),
-    # url(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
+    path('', list_views.home_page, name='home'),
+    path('lists/', include(list_urls)),
 ]
